d94dcb75a1c3b3b5faa686892e60a90f965f16c9
Harsh Kedia <hkedia@hey.com>
2024-08-06 17:11:39 +0530

    add logging

--- config/config.exs
+++ config/config.exs
@@ -1,2 +1,9 @@
 import Config
-config :issues, github_url: "https://api.github.com"
+
+config :issues,
+  github_url: "https://api.github.com"
+
+config :logger,
+  compile_time_purge_matching: [
+    [module: Issues.GithubIssues, level_lower_than: :info]
+  ]
